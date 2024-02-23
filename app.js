87ecc11607d02ba2d1a950ecd6a11993a68c7823
Miriam Aguilar <m2002aguilar@gmail.com>
2024-02-23 10:17:44 -0800

    login endpoint

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
-//const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const jsonParser = bodyParser.json();
 
 const { MongoClient } = require('mongodb');
@@ -29,54 +29,54 @@ app.get("/", (request, response, next) => {
   });  
 
 //login endpoint 
-// async function loginUser(request, response){
-// 	User
-// 	.findOne({ email: request.body.email })
-// 	.then((user) => {
-// 		bcrypt.compare(request.body.password, user.password)
-// 		.then((passwordCheck) => {
+async function loginUser(request, response){
+	User
+	.findOne({ email: request.body.email })
+	.then((user) => {
+		bcrypt.compare(request.body.password, user.password)
+		.then((passwordCheck) => {
 
-// 			//check if password matches
-// 			if(!passwordCheck){
-// 				return resposnse.status(400).send({
-// 					message: "Passwords do not match", 
-// 					error
-// 				})
-// 			}
+			//check if password matches
+			if(!passwordCheck){
+				return resposnse.status(400).send({
+					message: "Passwords do not match", 
+					error
+				})
+			}
 
-// 			//create JWT token
-// 			const token = jwt.sign(
-// 				{
-// 					userId: user._id,
-// 					userEmail: user.email
-// 				},
-// 				"RANDOM-TOKEN",
-// 				{ expiresIn: "24h"}
-// 			);
+			//create JWT token
+			const token = jwt.sign(
+				{
+					userId: user._id,
+					userEmail: user.email
+				},
+				"RANDOM-TOKEN",
+				{ expiresIn: "24h"}
+			);
 
-// 			//return success response 
-// 			response.status(200).send({
-// 				message: "Login Successful",
-// 				email: user.email,
-// 				token
-// 			})
-// 		})
-// 		.catch((error) => {
-// 			response.status(400).send({
-// 				message: "Passwords do not match",
-// 				error
-// 			});
-// 		});
-// 	})
-// 	.catch((e) => {
-// 		response.status(404).send({
-// 			message: "Email not found",
-// 			e
-// 		});
-// 	});
+			//return success response 
+			response.status(200).send({
+				message: "Login Successful",
+				email: user.email,
+				token
+			})
+		})
+		.catch((error) => {
+			response.status(400).send({
+				message: "Passwords do not match",
+				error
+			});
+		});
+	})
+	.catch((e) => {
+		response.status(404).send({
+			message: "Email not found",
+			e
+		});
+	});
 
-// }
-// app.post("/api/login", loginUser);
+}
+app.post("/api/login", loginUser);
 
 //register endpoint
 async function registerUser(request, response){
